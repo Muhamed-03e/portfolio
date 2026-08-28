62871b842a4a2c056de31f179be2d8d513d004b1
muhamed <bheryahmed024@gmail.com>
2026-08-28 16:37:19 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -10,3 +10,8 @@ const nextConfig: NextConfig = {
         formats: ["image/avif", "image/webp"],
         unoptimized: true,
     },
+    serverExternalPackages: ["firebase-admin"],
+    trailingSlash: true,
+};
+
+export default nextConfig;
